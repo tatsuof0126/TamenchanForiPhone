6d60b24617fcbad7de56af1fbfcdf46a5a6d2665
tatsuof0126 <tatsuof0126@gmail.com>
2012-10-20 00:32:03 +0900

    Initial Commit

---
+++ Tamenchan/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Tamenchan
+//
+//  Created by 藤原 達郎 on 2012/10/20.
+//  Copyright (c) 2012年 Tatsuo Fujiwara. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ TamenchanTests/TamenchanTests.m
@@ -0,0 +1,32 @@
+//
+//  TamenchanTests.m
+//  TamenchanTests
+//
+//  Created by 藤原 達郎 on 2012/10/20.
+//  Copyright (c) 2012年 Tatsuo Fujiwara. All rights reserved.
+//
+
+#import "TamenchanTests.h"
+
+@implementation TamenchanTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TamenchanTests");
+}
+
+@end
